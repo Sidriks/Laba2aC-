cb203d4fe24fa360997cd1ec140b1fb60ed7e7fe
Sidriks <61543870+Sidriks@users.noreply.github.com>
2020-04-12 23:21:54 +0300

    Add files via upload

---
+++ laba2a.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Circle circle = new Circle();
+            Console.WriteLine("Input Radius: ");
+            double Radius;
+            circle.Radius = double.Parse(Console.ReadLine());
+           
+            Console.WriteLine($"Square = {circle.getSquare()}");
+            Console.WriteLine($"Circumference = {circle.getCircumference()}");
+            Console.ReadKey();
+
+        }
+    }
+}
